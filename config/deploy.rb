f068b7f7399fa508bd6027d25924c6b3ffed5385
Bryan Prather-Huff <pratherhuff@gmail.com>
2017-01-08 14:36:50 -0600

    fixing host

---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+# config valid only for current version of Capistrano
+lock "3.7.1"
+
+ require 'capistrano/deploy'
+
+set :repo_url, "git@github.com:bryandph/farms.casa.git"
+
+# replace this with your site's name
+set :application, "farms.casa"
+set :repository, '_site'
+set :scm, :none
+set :deploy_via, :copy
+set :copy_compression, :gzip
+set :use_sudo, false
+
+# the name of the user that should be used for deployments on your VPS
+set :user, "deployer"
+
+# the path to deploy to on your VPS
+set :deploy_to, "/var/www/farms.casa"
+
+# the ip address of your VPS
+role :web, "accentii.xyz"
+
+before 'deploy:update', 'deploy:update_jekyll'
+
+namespace :deploy do
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update do
+    # clear existing _site
+    # build site using jekyll
+    # remove Capistrano stuff from build
+    %x(rm -rf _site/* && jekyll build && rm _site/Capfile && rm -rf _site/config)
+  end
+end
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
